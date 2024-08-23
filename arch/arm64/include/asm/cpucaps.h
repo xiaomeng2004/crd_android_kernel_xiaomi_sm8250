f59a9707f0cbf330f0e5d5b1306d4f084b969429
Rohail33 <rohailyousafzai49@gmail.com>
2024-08-23 18:28:07 +0200

    Merge tag 'v4.19.320' of https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux
    This is the 4.19.320 stable release

--- arch/arm64/include/asm/cpucaps.h
+++ arch/arm64/include/asm/cpucaps.h
@@ -60,6 +60,7 @@
 #define ARM64_HAS_CRC32				39
 #define ARM64_HAS_SB				40
 #define ARM64_WORKAROUND_1742098		41
+#define ARM64_WORKAROUND_SPECULATIVE_SSBS	42
 
 /* kabi: reserve 42 - 62 for future cpu capabilities */
 #define ARM64_NCAPS				62
